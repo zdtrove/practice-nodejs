e83c5fd4f640a1a937cdcf12506e331710783046
Your Name <you@example.com>
2019-11-08 00:22:13 +0700

    commit

---
+++ Shape/rectangle.js
@@ -0,0 +1,17 @@
+// exports.area = (width, height) => width * height;
+// exports.circumference = (width, height) => 2 * (width + height);
+
+// module.exports = {
+//     area: (width, height) => width * height,
+//     circumference: (width, height) => 2 * (width + height)
+// }
+
+module.exports = {
+    area: (width, height) => {
+        return width * height;
+    },
+    circumference: (width, height) => {
+        return 2 * (width + height);
+    },
+    currentDateTime: Date()
+}
--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log('Nodejs');
+const rect = require('./Shape/rectangle');
+console.log(`The area is: ${rect.area(10, 20)}`);
+console.log(`The circumference is: ${rect.circumference(10, 20)}`);
+console.log(`Current time is: ${rect.currentDateTime}`);
