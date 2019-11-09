e02f479dbdede8bfc5983fc6c44088572e319237
Your Name <you@example.com>
2019-11-09 18:54:13 +0700

    com

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Nodejs');
+let http = require('http');
+const port = 3001;
+const server = http.createServer((request, response) => {
+    // response.write('This is a response for a request');
+    response.writeHead(200, {
+        'Content-Type': 'text/html',
+        'Trailer': 'Content-MD5'
+    });
+    const ipAddress = request.socket.remoteAddress;
+    response.write(`Your IP address is ${ipAddress} \r\n`);
+    response.write(`Your request's url is ${request.url}`);
+    response.write(`Detail request's url ${require('url').parse(request.url, true)}`);
+    debugger;
+    response.addTrailers({'Content-MD5': '7895bf4b8828b55ceaf47747b4bca667'});
+    response.end();
+}).listen(port);
+console.log(`Server is running on port: ${port}`);
