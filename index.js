a9f4183f4906d0259f92a2c7fcc93edc04ade6bd
Your Name <you@example.com>
2019-11-09 15:17:11 +0700

    cm

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log('Nodejs');
+debugger;
+var arithmeticMean = (numbers) => {
+    var total = 0;
+    debugger;
+    for (let number in numbers) {
+        total = total + parseFloat(number);
+        console.log(`total is ${total}, number is: ${number}`);
+    }
+    console.log(`Result is: ${total / numbers.length}`);
+}
+arithmeticMean([1,2,3,4,5]);
