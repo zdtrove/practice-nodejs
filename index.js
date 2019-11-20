60e1c48ba31714dc5df4d327ad295a0022c308e6
Hoang An <tonnguyenhoangan@gmail.com>
2019-11-20 14:03:40 +0700

    cm

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const Logger = require('./logger');
+const http = require('http');
+// const path = require('path');
+// const fs = require('fs');
 
-const logger = new Logger();
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end('<h1>Homeff</h1>');
+    }
+});
 
-logger.on('message', data => console.log('Called Listener', data));
-logger.log('Hello World');
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- logger.js
+++ logger.js
@@ -10,6 +10,8 @@ class Logger extends EventEmitter {
 
 // module.exports = Logger;
 
+const Logger = require('./logger');
+
 const logger = new Logger();
 
 logger.on('message', data => console.log('Called Listener', data));
